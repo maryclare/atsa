6438b08bbec6c23d427f4232c49734e6375ffc0b
Maryclare Griffin <maryclaregriffin@gmail.com>
2019-02-20 21:09:34 -0500

    Some updates to typos and additions to code examples for Thursday

--- content/code/code_2.R
+++ content/code/code_2.R
@@ -74,8 +74,103 @@ est.innov <- function(x) {
 # This gives us estimates of the vector d_n, which in turn gives us an estimate of psi
 psi.hat <- est.innov(x)$d.n
 plot(psi.hat, type = "l", ylab = expression(hat(psi)))
+
 # We can then back out the MA parameters, but we won't do that here since
 # you're probably never going to need to do approximate method of moments estimation
 # for MA(q), ARMA(p, q) models in practice
+acf(x, lag.max = 50)
+abline(v = 27, lty = 3, col = "gray")
+# This suggests an MA q model with q = 27
+q <- 27
+# With no AR terms, the thetas are the psis!
+theta.hat <- psi.hat[1:q]
+# Get the noise variance from computing E[x_t^2]
+sig.sq.w.hat <- acf(x, lag.max = 0, type = "cov", plot = FALSE)$acf[1, 1, 1]/(1 + sum(theta.hat^2))
+mu.x.hat <- mean(x) # Again, this is out moment estimator of the covariance
 
+# What if we wanted to fit an ARMA model? Use p = 12, q = 27
+# First, compute phis based on c = D phi, where c is the q+1,...,q+p terms of psi.hat
+# and elements of D are elements of psi
+c <- psi.hat[(q + 1):(p + q)]
+D <- matrix(0, nrow = p, ncol = p)
+for (j in 1:p) {
+  for (i in 1:p) {
+    if (i <= min(j, p)) {
+      D[j, i] <- psi.hat[q + j - i]
+    }
+  }
+}
+phi.hat <- solve(D)%*%c
 
+E <- matrix(0, nrow = q, ncol = p)
+for (j in 1:q) {
+  for (i in 1:p) {
+    if (i <= min(j, p)) {
+      if (j - i == 0) {
+        E[j, i] <- 1
+      } else {
+        E[j, i] <- psi.hat[j - i]
+      }
+    }
+  }
+}
+theta.hat <- psi.hat[1:q] - E%*%phi.hat
+
+plot(phi.hat, type = "b")
+plot(theta.hat, type = "b")
+# These look pretty crazy...
+
+# We know that these aren't going to be very good estimators - let's move on
+# to maximum likelihood methods. These are implemented by the arima function
+?arima
+
+# The default fits the model using unconditional maximum likelihood
+uml <- arima(x, order = c(2, 0, 3), method = "ML")
+# Could try the easy thing instead
+cls <- arima(x, order = c(2, 0, 3), method = "CSS")
+# Also has a conditional-maximum likelihood method - 
+# This throws out the first p+q observations but not the 
+# log(r) term. We haven't talked about this, so we won't try it out
+cbind(coef(uml), coef(cls))
+# They look similar!
+
+# We get an asymptotic variance covariance matrix 
+uml$var.coef
+# This is where our standard errors come from
+sqrt(diag(uml$var.coef))
+uml
+# And - we get a noise variance estimate
+uml$sigma2
+
+# Let's try picking the ARMA(p, q) order!
+ps <- 0:8
+qs <- 0:8
+aic <- aicc <- sic <- matrix(nrow = length(ps), ncol = length(qs))
+
+for (i in 1:length(ps)) {
+  cat("i=", i, "\n")
+  for (j in 1:length(qs)) {
+    p <- ps[i]
+    q <- qs[j]
+    uml <- arima(x, order = c(p, 0, q), method = "ML")
+    sig.sq <- uml$sigma2
+    aic[i, j] <- log(sig.sq) + (n + 2*(p + q + 1))/n
+    aicc[i, j] <- log(sig.sq) + (n + p + q + 1)/(n - p - q - 1 - 2)
+    sic[i, j] <- log(sig.sq) + (p + q + 1)*log(n)/(n)
+  }
+}
+
+aic.pq <- which(aic == min(aic), arr.ind = TRUE)
+aicc.pq <- which(aicc == min(aicc), arr.ind = TRUE)
+sic.pq <- which(sic == min(sic), arr.ind = TRUE)
+c(ps[aic.pq[1]], qs[aic.pq[2]])
+c(ps[aicc.pq[1]], qs[aicc.pq[2]])
+c(ps[sic.pq[1]], qs[sic.pq[2]])
+
+# What if we really underfit the model?
+uml <- arima(x, order = c(2, 0, 2), method = "ML")
+# Can look at the residuals
+plot(uml$residuals, ylab = "Residuals")
+acf(uml$residuals, main = "Residuals")
+acf(uml$residuals, type = "partial", main = "Residuals")
+# Eeep! There's still a lot of temporal dependence left!
