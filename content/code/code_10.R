b5b12ca45d7b3b35df5494c7e14b2feb65a167d0
Maryclare Griffin <maryclaregriffin@gmail.com>
2019-04-24 09:12:54 -0400

    Updated HW 10

--- content/code/code_10.R
+++ content/code/code_10.R
@@ -47,37 +47,3 @@ lines((length(to.use) + 1):n, fitted(forc), col = "red")
 lines((length(to.use) + 1):n, colMeans(pred$y), col = "blue")
 lines((length(to.use) + 1):n, apply(pred$y, 2, function(x){quantile(x, 0.025)}), col = "blue", lty = 2)
 lines((length(to.use) + 1):n, apply(pred$y, 2, function(x){quantile(x, 0.975)}), col = "blue", lty = 2)
-
-# How about we add an AR(1) process for the mean?
-spec11 <- ugarchspec(variance.model=list(model="sGARCH", garchOrder=c(1, 0)),
-                     mean.model=list(armaOrder=c(1, 0, 0), 
-                                     # Note, these are ARFIMA parameter values not ARFIMA, 
-                                     # always keep the middle value zero unless you learn later about
-                                     # ARFIMA models later and want to fit them!
-                                     include.mean=TRUE), distribution.model = "norm")
-# Let's fit our simple GARCH Model
-fit11 <- ugarchfit(data=y,spec=spec11)
-n.ahead <- length((length(to.use) + 1):n)
-forc <- ugarchforecast(fit11, n.ahead = n.ahead)
-
-# Now lets simulate from the posterior of our simple stochastic volatility model
-res <- svsample(y, designmatrix = "ar1")
-pred <- predict(res, n.ahead)
-names(summary(res))
-
-
-summary(res, showlatent=FALSE)
-
-plot(c(nyse)^2, type = "l", col = "gray", xlim = c(length(nyse) - 2*n.ahead, length(nyse)), ylim = c(0, 0.0015))
-lines(y^2, col = "black")
-lines((length(to.use) + 1):n, sigma(forc)^2, col = "red")
-lines((length(to.use) + 1):n, colMeans(exp(pred$h)), col = "blue")
-
-
-plot(c(nyse), type = "l", col = "gray", xlim = c(length(nyse) - 2*n.ahead, length(nyse)), ylim = c(-1, 1)*0.06)
-lines(y, col = "black")
-lines((length(to.use) + 1):n, fitted(forc), col = "red")
-lines((length(to.use) + 1):n, colMeans(pred$y), col = "blue")
-lines((length(to.use) + 1):n, apply(pred$y, 2, function(x){quantile(x, 0.025)}), col = "blue", lty = 2)
-lines((length(to.use) + 1):n, apply(pred$y, 2, function(x){quantile(x, 0.975)}), col = "blue", lty = 2)
-
