5cb273af9383d615fd5fb0e37977c90227e70548
Maryclare Griffin <maryclaregriffin@gmail.com>
2019-05-09 18:07:02 -0400

    Fixed one last issue in code_16.R (hopefully)

--- content/code/code_16.R
+++ content/code/code_16.R
@@ -13,11 +13,15 @@ X <- X[1:400, ]
 
 # This software only lets us add covariates to the VAR model
 Z <- 1:length(rec)
+Z.1 <- Z[1:nrow(X)]
+Z.2 <- Z[(nrow(X) + 1):length(Z)]
 
-varx <- VARX(X, p = 1, xt = Z, include.mean = FALSE, m = 0)
+
+varx <- VARX(X, p = 1, xt = Z.1, include.mean = FALSE, m = 0)
 # Hard to get predictions out...
 
-linfit <- lm(X~Z)
+
+linfit <- lm(X~Z.1)
 
 armaMTS <- VARMA(linfit$residuals, p = 1, q = 1, include.mean = FALSE)
 armaMTS$Phi
@@ -29,13 +33,13 @@ plot(c(rec), col = "gray", ylim = c(-200, 200), type = "l", axes = FALSE,
      xlab = "Time", ylab = "Recruitment")
 lines(X[, 2])
 # We'll need to add the fitted values onto our forecasts
-lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)), col = "blue")
-lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)) + qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
+lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*Z.2, col = "blue")
+lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*Z.2 + qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
       lty = 2)
-lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2]+ linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)) - qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
+lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2]+ linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*Z.2 - qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
       lty = 2)
 # Add linear model fit line just to get a sanity check
-abline(a = linfit$coefficients[, 2][1], b = linfit$coefficients[, 2][2], lty = 2)
+abline(a = linfit$coefficients[, 2][1], b = linfit$coefficients[, 2][2], lty = 1, col = "red")
 axis(2, seq(-200, 200, by = 50))
 axis(1, seq(0, length(rec) + 1, by = 50), c("", time(rec), "")[seq(0, length(rec) + 1, by = 50) + 1])
 
@@ -63,7 +67,7 @@ lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + Z.2%*%linfit$coefficient
 lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2]+ Z.2%*%linfit$coefficients[, 2] - qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
       lty = 2)
 # Add linear model fit line just to get a sanity check
-lines(Z.1%*%linfit$coefficients[, 2], lty = 2)
+lines(Z.1%*%linfit$coefficients[, 2], lty = 1, col = "red")
 axis(2, seq(-200, 200, by = 50))
 axis(1, seq(0, length(rec) + 1, by = 50), c("", time(rec), "")[seq(0, length(rec) + 1, by = 50) + 1])
 
