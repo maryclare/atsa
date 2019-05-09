74b58bc66d7c0f8975c90910895c4d5427e45626
Maryclare Griffin <maryclaregriffin@gmail.com>
2019-05-09 10:21:56 -0400

    Had an issue in plotting code

--- content/code/code_16.R
+++ content/code/code_16.R
@@ -12,7 +12,7 @@ X <- cbind(soi, rec)
 X <- X[1:400, ]
 
 # This software only lets us add covariates to the VAR model
-Z <- 1:nrow(X)
+Z <- 1:length(rec)
 
 varx <- VARX(X, p = 1, xt = Z, include.mean = FALSE, m = 0)
 # Hard to get predictions out...
@@ -25,12 +25,16 @@ armaMTS$Theta
 
 varma.pred <- VARMApred(armaMTS, h = length(soi) - nrow(X))
 
-plot(rec, col = "gray", ylim = c(-200, 200))
-lines(time(rec)[1:nrow(X)], X[, 2])
+plot(c(rec), col = "gray", ylim = c(-200, 200), type = "l", axes = FALSE, 
+     xlab = "Time", ylab = "Recruitment")
+lines(X[, 2])
 # We'll need to add the fitted values onto our forecasts
-lines(time(rec)[(nrow(X) + 1):length(rec)], varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)), col = "blue")
-lines(time(rec)[(nrow(X) + 1):length(rec)], varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)) + qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
+lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)), col = "blue")
+lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2] + linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)) + qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
       lty = 2)
-lines(time(rec)[(nrow(X) + 1):length(rec)], varma.pred$pred[, 2]+ linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)) - qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
+lines((nrow(X) + 1):length(rec), varma.pred$pred[, 2]+ linfit$coefficients[, 2][1] + linfit$coefficients[, 2][2]*(nrow(X) + 1:nrow(varma.pred$pred)) - qnorm(0.975)*varma.pred$se.err[, 2], col = "blue",
       lty = 2)
-
+# Add linear model fit line just to get a sanity check
+abline(a = linfit$coefficients[, 2][1], b = linfit$coefficients[, 2][2], lty = 2)
+axis(2, seq(-200, 200, by = 50))
+axis(1, 0:length(rec), c("", time(rec)))
